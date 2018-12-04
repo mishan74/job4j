065f33380d6fd60412f456ffed5172b45505352c
Mikhail Rozdin <mrozdin@yandex.ru>
2018-12-04 13:40:10 +0300

    1.4. JUnit[#92383]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -10,7 +10,17 @@ public class Calculate{
 	*Метод для вывода в консоль "Hello world"
 	*@param args - args
 	*/
-	public static void main(String...args){
+	public static void main(String...args) {
 		System.out.println("Hello world");
 	}
+	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+ 
 }
