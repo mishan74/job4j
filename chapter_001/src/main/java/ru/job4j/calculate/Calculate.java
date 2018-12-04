df7ec7c0e919e591d5cec1a6e0102a8d903dca0f
Mikhail Rozdin <mrozdin@yandex.ru>
2018-12-04 11:38:59 +0300

    1.3 JavaDoc. Code convention[#92382]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+*Class Calculate решение заданий части 001
+*@author Mikhail Rozdin
+*@since 04.12.2018
+*/
 public class Calculate{
+	/**
+	*Метод для вывода в консоль "Hello world"
+	*@param args - args
+	*/
 	public static void main(String...args){
 		System.out.println("Hello world");
 	}
