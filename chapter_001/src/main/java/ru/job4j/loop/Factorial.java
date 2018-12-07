1e94f5b70ed918c73cbdd1a1c5071ab79260812f
Mikhail Rozdin <mrozdin@yandex.ru>
2018-12-07 08:51:54 +0300

    5.2. Создать программу вычисляющую факториал.[#92354]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+/**
+ * Class Factorial рассчет факториала числа
+ * @author Mikhail Rozdin
+ * @since 07.12.2018
+ */
+public class Factorial {
+
+    /**
+     * Метод calc рассчитывает факториал.
+     * @param n число, для которого считать факториал.
+     * @return факториал положительного числа,
+     * 1 если n = 0,
+     * 0 если n < 0.
+     */
+    public int calc(int n) {
+        int result = 1;
+        if (n < 0) {
+            result = 0;
+        }
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
