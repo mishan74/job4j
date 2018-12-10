51c832f7328cad4890698b32b6d6edf251d80226
Mikhail Rozdin <mrozdin@yandex.ru>
2018-12-10 13:50:02 +0300

    5.5. Рефакторинг кода.[#92352]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class Paint draw pyramid
  * @author Mikhail Rozdin
@@ -8,16 +10,55 @@ package ru.job4j.loop;
 public class Paint {
 
     /**
+     * метод прорисолвки правостороннего треугольника.
+     * @param height высота треугольника.
+     * @return треугольник в псевдо графике.
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * метод прорисолвки левостороннего треугольника.
+     * @param height высота треугольника.
+     * @return треугольник в псевдо графике.
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+    /**
      * Method draw pyramid
      * @param height height pyramid
      * @return pseudo graphics pyramid string.
      */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    /**
+     * Method loopBy содержит главную логику построения пирамиды.
+     * @param height высоат пирамиды.
+     * @param weight ширина пирамиды.
+     * @param predict условие проставление галки.
+     * @return пирамида в псевдо графике.
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
