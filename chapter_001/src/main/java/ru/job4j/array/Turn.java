f14968d71ea25a471ec3fe7c5d41ed26f15f6b8a
Mikhail Rozdin <mrozdin@yandex.ru>
2018-12-10 16:25:42 +0300

    6.2. Перевернуть массив.[#92368]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ *Class Turn переворачивает массив.
+ *@author Mikhail Rozdin
+ *@since 10.12.2018
+ */
+public class Turn {
+
+    /**
+     * Метод, переворачивающий значения массива.
+     * @param array целочисленный массив.
+     * @return перевернутый целочисленный массив.
+     */
+    public int[] back(int[] array) {
+        for (int i = 0; i < array.length / 2 ; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return  array;
+    }
+}
