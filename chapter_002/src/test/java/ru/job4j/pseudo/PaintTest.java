ba39ae50e9d8c5d937a64f13fc920ebc80e5ce89
Mikhail Rozdin <mrozdin@yandex.ru>
2018-12-24 14:45:02 +0300

    5. Рефакторинг теста @Before @After[#92417]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -14,11 +16,37 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class PaintTest {
+
+    /**
+     * поле содержит дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+
+    /**
+     * буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    /**
+     * Замена дефолтного потока вывода.
+     */
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    /**
+     * Установка потоку вывода дефолтное значение.
+     */
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -31,14 +59,10 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
@@ -50,6 +74,5 @@ public class PaintTest {
                                 .add("")
                                 .toString()
         ));
-        System.setOut(stdout);
     }
 }
