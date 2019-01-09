0200d5a69d4041631c9e996653bd7203067442a2
Mikhail Rozdin <mrozdin@yandex.ru>
2019-01-09 12:13:01 +0300

    3. Конвертация двумерного массива в ArrayList[#92407]

--- chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -31,7 +31,7 @@ public class ConvertMatrix2ListTest {
                     {93, 54, 25, 16},
             };
             List<Integer> expect = Arrays.asList(
-                    1, 2, 8, 9, 3, 4, 5, 6, 36, 41 ,52, 63, 93, 54, 25, 16
+                    1, 2, 8, 9, 3, 4, 5, 6, 36, 41, 52, 63, 93, 54, 25, 16
             );
             List<Integer> result = list.toList(input);
             assertThat(result, is(expect));
