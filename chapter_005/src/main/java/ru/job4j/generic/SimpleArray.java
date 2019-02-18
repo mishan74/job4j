8e311cba003aad41fb8cf815fd285596304193a5
Mikhail Rozdin <mrozdin@yandex.ru>
2019-02-18 15:19:30 +0300

    5.2.2. Реализовать Store<T extends Base>[#107421] boolean replace не дублирует значения, проверка на позицию индекса

--- chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -21,10 +21,10 @@ public class SimpleArray<T> implements Iterable<T> {
     public T set(int index, T model) throws ArrayIndexOutOfBoundsException {
         checkException(index);
         T result = null;
-        int position = Arrays.asList(objects).indexOf(model);
-        if (position != -1) {
+        int positionModel = Arrays.asList(objects).indexOf(model);
+        if (positionModel != -1 && position > index) {
             objects[index] = model;
-            remove(position);
+            remove(positionModel);
             result = model;
         }
         return result;
