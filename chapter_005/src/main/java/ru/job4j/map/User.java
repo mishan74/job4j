e31a7f422c7cce87d89f7d837e33eb0dc765d297
Mikhail Rozdin <mrozdin@yandex.ru>
2019-02-22 16:04:35 +0300

    3. Переопределить только hashCode[#107429]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 /**
  * @author Mikhail Rozdin
@@ -16,5 +17,32 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+   // @Override
+   // public boolean equals(Object o) {
+   //     if (this == o) return true;
+   //     if (o == null || getClass() != o.getClass()) return false;
+   //     User user = (User) o;
+   //     return children == user.children &&
+   //             Objects.equals(name, user.name) &&
+   //             Objects.equals(birthday, user.birthday);
+   // }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
