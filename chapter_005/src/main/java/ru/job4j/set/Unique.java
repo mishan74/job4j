8c7de629f488684d49664e17d7719c17496f61a1
Mikhail Rozdin <mrozdin@yandex.ru>
2019-03-04 10:06:43 +0300

    Проверка уникальности символов строк

---
+++ chapter_005/src/main/java/ru/job4j/set/Unique.java
@@ -0,0 +1,30 @@
+package ru.job4j.set;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Mikhail Rozdin
+ * @version $Id$
+ * @since 0.1
+ */
+
+
+public class Unique {
+    /**
+     * Проверка уникальности символов строк
+     */
+    public boolean isUnique(String first, String second) {
+        int checkSize;
+        Set<Character> uniqueSet = new HashSet<>();
+        for (char a : first.toCharArray()) {
+            uniqueSet.add(a);
+        }
+        checkSize = uniqueSet.size();
+        for (char a : second.toCharArray()) {
+            uniqueSet.add(a);
+        }
+        return checkSize == uniqueSet.size();
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/set/UniqueTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Mikhail Rozdin
+ * @version $Id$
+ * @since 0.1
+ */
+public class UniqueTest {
+    Unique unique;
+    @Before
+    public void init() {
+        unique = new Unique();
+    }
+
+    @Test
+    public void whenUniqueThenTrue() {
+        String first = "Hello";
+        String second = "He";
+        assertThat(unique.isUnique(first, second), is(true));
+    }
+
+    @Test
+    public void whenNotUniqueThenFalse() {
+        String first = "Hello";
+        String second = "he";
+        assertThat(unique.isUnique(first, second), is(false));
+    }
+}
