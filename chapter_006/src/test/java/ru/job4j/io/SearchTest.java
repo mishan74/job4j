4a8dc0e9182dfd0175d096aefd21561b9bc376f7
Mikhail Rozdin <mrozdin@yandex.ru>
2019-03-12 22:16:35 +0300

    3. Сканирование файловой системы.[#107479] Доработка

--- chapter_006/src/test/java/ru/job4j/io/SearchTest.java
+++ chapter_006/src/test/java/ru/job4j/io/SearchTest.java
@@ -21,47 +21,18 @@ public class SearchTest {
         System.out.println(path);
 
         File temp = new File(path + "\\javatest");
-        temp.mkdir();
+        boolean dir1 = temp.mkdir();
         temp = new File(path + "\\javatest\\test");
-        temp.mkdir();
-        temp = new File(path + "\\javatest\\test\\one");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\one\\two");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\one\\three");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\one//four");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\tempdir");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\tempdir\\one");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\tempdir\\two");
-        temp.mkdir();
-        temp  = new File(path + "\\javatest\\test\\tempdir\\two\\three");
-        temp.mkdir();
-
+        boolean dir2 = temp.mkdir();
         try (
-                FileOutputStream one = new FileOutputStream(path + "\\javatest\\test\\one\\one.txt");
-                FileOutputStream two = new FileOutputStream(path + "\\javatest\\test\\one\\two.rtf");
-                FileOutputStream three = new FileOutputStream(path + "\\javatest\\test\\one\\three\\three.txt");
-                FileOutputStream four = new FileOutputStream(path + "\\javatest\\test\\tempdir\\one\\four.txt");
-                FileOutputStream five = new FileOutputStream(path + "\\javatest\\test\\five.txt");
-                FileOutputStream six = new FileOutputStream(path + "\\javatest\\test\\tempdir\\two\\six.jpeg");
-                FileOutputStream seven = new FileOutputStream(path + "\\javatest\\test\\tempdir\\two\\seven.txt");
-                FileOutputStream eight = new FileOutputStream(path + "\\javatest\\test\\tempdir\\two\\three\\eight.txt");
-                FileOutputStream nine = new FileOutputStream(path + "\\javatest\\test\\tempdir\\two\\three\\nine.rtf")
+                FileOutputStream one = new FileOutputStream(path + "\\javatest\\test\\one.txt");
+                FileOutputStream two = new FileOutputStream(path + "\\javatest\\test\\two.rtf");
+                FileOutputStream six = new FileOutputStream(path + "\\javatest\\six.jpeg")
 
         ) {
             one.write("Text".getBytes());
             two.write("Text".getBytes());
-            three.write("Text".getBytes());
-            four.write("Text".getBytes());
-            five.write("Text".getBytes());
             six.write("Text".getBytes());
-            seven.write("Text".getBytes());
-            eight.write("Text".getBytes());
-            nine.write("Text".getBytes());
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -73,14 +44,13 @@ public class SearchTest {
         List<String> check = Arrays.asList(".txt", ".jpeg");
         List<File> files = search.files(path, check);
         List<String> expected = Arrays.asList(
-                "five.txt", "one.txt", "three.txt", "four.txt", "seven.txt", "six.jpeg", "eight.txt"
+                 "six.jpeg", "one.txt"
         );
         List<String> result = new ArrayList<>();
         for (File file : files) {
            result.add(file.getName());
         }
+
         assertThat(result, is(expected));
-        File temp = new File(path + "\\javatest");
-        temp.delete();
     }
 }
